d22ddb864762b4c4ce9b45cbf2a4e77c4ede0097
Andrew Plaza <github@andrewplaza.dev>
2026-02-20 14:49:16 -0500

    font

--- config.el
+++ config.el
@@ -38,14 +38,13 @@
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+(setq display-line-numbers-type 'relative)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
 (setq evil-escape-key-sequence "jk")
-(setq display-line-numbers-type 'relative)
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `with-eval-after-load' block, otherwise Doom's defaults may override your
 ;; settings. E.g.
